05d57269a4fe8298cfabb05f0f288c726e800e63
niludarekar1 <nilu.darekar1@gmail.com>
2022-08-07 16:56:20 +0530

    Added:Diffrent Rest

--- src/main/java/com/bridgelabz/demo/controller/GreetingControllerDemo.java
+++ src/main/java/com/bridgelabz/demo/controller/GreetingControllerDemo.java
@@ -4,6 +4,8 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,22 @@ public class GreetingControllerDemo {
 	        return new Greeting(counter.incrementAndGet(),
 	                String.format(template, name));
 	    }
+	    
+	  
+	    @RequestMapping("/user")
+	    public Greeting greetingUser(@RequestParam(value="firstname", defaultValue = "Guest") String firstname,
+	                             @RequestParam(value="lastname", defaultValue = "User") String lastname) {
+	        return new Greeting(counter.incrementAndGet(),
+	                String.format(template, (firstname+" "+lastname)));
+	    }
+
+	    @PutMapping("/{firstname}")
+	    public Greeting greeting(@PathVariable String firstname,
+	                             @RequestParam(value = "lastname", defaultValue = "User") String lastname) {
+	        return new Greeting(counter.incrementAndGet(),
+	                String.format(template, (firstname+" "+lastname)));
+	    }
+
 	}
 
 
